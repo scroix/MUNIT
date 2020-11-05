b6949a4c3fa553db1778acec60d9f4504e0580e2
Julien de-Sainte-Croix <julien.dsc@gmail.com>
2020-11-05 13:02:35 +1100

    Use generic tag for docker build/run shortcut

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-if [[ "$(docker images -q munit:1.2 2> /dev/null)" == "" ]]; then
-  docker build --tag munit:1.2 .
+if [[ "$(docker images -q munit-cuda11 2> /dev/null)" == "" ]]; then
+  docker build --tag munit-cuda11 .
 else
-  docker run -v $(pwd):/app --runtime=nvidia -it munit:1.2 /bin/bash
+  docker run -v $(pwd):/app --runtime=nvidia -it munit-cuda11 /bin/bash
 fi
